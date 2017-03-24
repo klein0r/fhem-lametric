b34a3148a0c66b6aede019bd224881ec49168167
Matthias Kleine <matthias@klein0r.de>
2017-03-24 21:55:07 +0100

    Updated controls

--- prepare_update.sh
+++ prepare_update.sh
@@ -3,7 +3,7 @@ rm controls_lametric.txt
 find ./FHEM -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f; 
   do
    echo "DEL ${f}" >> controls_lametric.txt
-   out="UPD "$(stat -c %y  $f | cut -d. -f1 | awk '{printf "%s_%s",$1,$2}')" "$(stat -c %s $f)" ${f}"
+   out="UPD "$(stat -f "%Sm" -t "%Y-%m-%d_%T" $f)" "$(stat -f%z $f)" ${f}"
    echo ${out//.\//} >> controls_lametric.txt
 done
 
